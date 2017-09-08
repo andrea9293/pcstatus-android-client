4f8833f4b8b7f098e324bcb48211de6f406a0f4e
Andrea <andreabravaccino@gmail.com>
2017-09-08 14:57:11 +0200

    cleaned code and added rescan and settings button

--- app/src/main/java/com/andrea/pcstatus/MainActivity.java
+++ app/src/main/java/com/andrea/pcstatus/MainActivity.java
@@ -4,50 +4,33 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.components.YAxis.AxisDependency;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements Observer, OnChartValueSelectedListener {
+public class MainActivity extends AppCompatActivity implements Observer {
 
     private TextView mTextMessage;
     private static final String TAG = "MainActivity";
     private static final int REQUEST_ENABLE_BT = 1;
-    private EditText editTextIP;
+    private static final int REQUEST_WIFI_OR_BLUETOOTH = 2;
     private WiFiConnectionController wiFiConnectionController;
-    private TimerTask task;
     private BluetoothConnectionController bluetoothConnectionController;
-    private Boolean isBluetoothConnection = false;
-    private Context context;
     private BottomNavigationView navigation;
     private LineChart mChart;
+    private LineChartMaker lineChartMaker;
+    private MenuItem rescanButton;
+    private boolean connectionFlag = false;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -56,21 +39,26 @@ public class MainActivity extends AppCompatActivity implements Observer, OnChart
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_miscellaneous:
-                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getMiscellaneous());
+                    if (SingletonBatteryStatus.getInstance().getMiscellaneous() != null)
+                        mTextMessage.setText(SingletonBatteryStatus.getInstance().getMiscellaneous());
                     mChart.setVisibility(LineChart.VISIBLE);
                     return true;
                 case R.id.navigation_hdd:
-                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getDisks());
+                    if (SingletonBatteryStatus.getInstance().getDisks() != null)
+                        mTextMessage.setText(SingletonBatteryStatus.getInstance().getDisks());
                     mChart.setVisibility(LineChart.INVISIBLE);
                     return true;
                 case R.id.navigation_pcinfo:
-                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getComputerInfo());
+                    if (SingletonBatteryStatus.getInstance().getComputerInfo() != null)
+                        mTextMessage.setText(SingletonBatteryStatus.getInstance().getComputerInfo());
                     return true;
                 case R.id.navigation_battery:
-                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getBattery());
+                    if (SingletonBatteryStatus.getInstance().getBattery() != null)
+                        mTextMessage.setText(SingletonBatteryStatus.getInstance().getBattery());
                     return true;
                 case R.id.navigation_cpu:
-                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getCpu());
+                    if (SingletonBatteryStatus.getInstance().getCpu() != null)
+                        mTextMessage.setText(SingletonBatteryStatus.getInstance().getCpu());
                     return true;
             }
             return false;
@@ -82,78 +70,27 @@ public class MainActivity extends AppCompatActivity implements Observer, OnChart
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context=getApplicationContext();
 
-        mChart = (LineChart) findViewById(R.id.chart1);
-        mChart.setOnChartValueSelectedListener(this);
+        Context context = getApplicationContext();
 
-        // enable description text
-        mChart.getDescription().setEnabled(true);
+        lineChartMaker = new LineChartMaker(MainActivity.this);
 
-        // enable touch gestures
-        mChart.setTouchEnabled(false);
-
-        // enable scaling and dragging
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        mChart.setDrawGridBackground(false);
-
-        // if disabled, scaling can be done on x- and y-axis separately
-        mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        mChart.setBackgroundColor(Color.LTGRAY);
-
-        LineData data = new LineData();
-        data.setValueTextColor(Color.WHITE);
-
-        // add empty data
-        mChart.setData(data);
-        // get the legend (only possible after setting data)
-        Legend l = mChart.getLegend();
-
-        // modify the legend ...
-        l.setForm(Legend.LegendForm.LINE);
-        l.setTypeface(Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf"));
-        l.setTextColor(Color.WHITE);
-
-        XAxis xl = mChart.getXAxis();
-        xl.setTypeface(Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf"));
-        xl.setTextColor(Color.WHITE);
-        xl.setDrawGridLines(false);
-        xl.setAvoidFirstLastClipping(true);
-        xl.setEnabled(true);
-
-        YAxis leftAxis = mChart.getAxisLeft();
-        leftAxis.setTypeface(Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf"));
-        leftAxis.setTextColor(Color.WHITE);
-        leftAxis.setAxisMaximum(100f);
-        leftAxis.setAxisMinimum(0f);
-        leftAxis.setDrawGridLines(true);
-
-        YAxis rightAxis = mChart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-
-
-
+        mChart = lineChartMaker.createLineChart();
 
         SingletonModel.getInstance().setSharedPreferences(getApplicationContext());
         SingletonBatteryStatus.getInstance().addingObserver(MainActivity.this);
-        wiFiConnectionController = new WiFiConnectionController(this);
-        //startWifi();
-        startBluetoothClient();
+        alertBox("Choose wifi or Bluetooth", "PCstatus need a connection via WiFi or bluetooth\n" +
+                "What you want to use?", REQUEST_WIFI_OR_BLUETOOTH);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        scheduleTask();
     }
 
-    public void startWifi(){
-        wiFiConnectionController.checkWifiForStats();
+    public void startWifiClient() {
+        taskCancel();
+        wiFiConnectionController = new WiFiConnectionController(MainActivity.this);
     }
-
 
     public void setTextView(String text) {
         mTextMessage.setText(text);
@@ -162,30 +99,36 @@ public class MainActivity extends AppCompatActivity implements Observer, OnChart
     @Override
     public void update(Observable observable, Object o) {
         refresh();
-       // setTextView(SingletonBatteryStatus.getInstance().getMiscellaneous());
+        lineChartMaker.addEntry();
     }
 
     private void refresh() {
         switch (navigation.getSelectedItemId()) {
             case R.id.navigation_miscellaneous:
-                mTextMessage.setText(SingletonBatteryStatus.getInstance().getMiscellaneous());
+                if (SingletonBatteryStatus.getInstance().getMiscellaneous() != null)
+                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getMiscellaneous());
                 break;
             case R.id.navigation_hdd:
-                mTextMessage.setText(SingletonBatteryStatus.getInstance().getDisks());
+                if (SingletonBatteryStatus.getInstance().getDisks() != null)
+                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getDisks());
                 break;
             case R.id.navigation_pcinfo:
-                mTextMessage.setText(SingletonBatteryStatus.getInstance().getComputerInfo());
+                if (SingletonBatteryStatus.getInstance().getComputerInfo() != null)
+                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getComputerInfo());
                 break;
             case R.id.navigation_battery:
-                mTextMessage.setText(SingletonBatteryStatus.getInstance().getBattery());
+                if (SingletonBatteryStatus.getInstance().getBattery() != null)
+                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getBattery());
                 break;
             case R.id.navigation_cpu:
-                mTextMessage.setText(SingletonBatteryStatus.getInstance().getCpu());
+                if (SingletonBatteryStatus.getInstance().getCpu() != null)
+                    mTextMessage.setText(SingletonBatteryStatus.getInstance().getCpu());
                 break;
         }
     }
 
     public void startBluetoothClient() {
+        taskCancel();
         bluetoothConnectionController = new BluetoothConnectionController(MainActivity.this);
     }
 
@@ -211,12 +154,12 @@ public class MainActivity extends AppCompatActivity implements Observer, OnChart
                 .setMessage(message + " Press OK to try again.")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface arg0, int arg1) {
-                        bluetoothConnectionController.bluetoothConnectionExecute();
+                        startBluetoothClient();
                     }
-                }).setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                }).setNegativeButton("CANCEL and try with WiFi", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                startWifi();
+                startWifiClient();
             }
         }).show();
     }
@@ -231,44 +174,42 @@ public class MainActivity extends AppCompatActivity implements Observer, OnChart
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
-            //bluetoothConnectionController.scheduleTask();
-            bluetoothConnectionController.getStatFromServer();
+            bluetoothConnectionController.scheduleTask();
         }
         if (resultCode == RESULT_CANCELED) {
+            taskCancel();
+            alertBox("Choose wifi or Bluetooth", "PCstatus need a connection via WiFi or bluetooth\n" +
+                    "What you want to use?", REQUEST_WIFI_OR_BLUETOOTH);
             //do nothing
         }
     }
 
-    private void scheduleTask() {
-        final Handler handler = new Handler();
-        Timer timer = new Timer();
-        Log.d(TAG, "task programmato");
-        task = new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    public void run() {
-                        Log.d(TAG, "task eseguito");
-                       // wiFiConnectionController.getStatsFromServer();
-                        bluetoothConnectionController.getStatFromServer();
-                        addEntry();
+
+    public void taskCancel() {
+        if (wiFiConnectionController != null) {
+            wiFiConnectionController.taskCancel();
+            wiFiConnectionController = null;
+        }
+        if (bluetoothConnectionController != null) {
+            bluetoothConnectionController.taskCancel();
+            bluetoothConnectionController = null;
+        }
+    }
+
+    public void alertBox(String title, String message, int REQUEST_WIFI_OR_BLUETOOTH) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Bluetooth", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        startBluetoothClient();
                     }
-                });
+                }).setNegativeButton("WiFi", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                startWifiClient();
             }
-        };
-        timer.schedule(task, 0, 1500); //it executes this every 1 minute
-    }
-
-    private void taskCancel() {
-        if (task != null)
-            bluetoothConnectionController.stopConnection();
-            task.cancel();
-    }
-
-    @Override
-    public void onBackPressed() {
-        taskCancel();
-        super.onBackPressed();
+        }).show();
     }
 
     @Override
@@ -278,62 +219,37 @@ public class MainActivity extends AppCompatActivity implements Observer, OnChart
     }
 
     @Override
-    public void onValueSelected(Entry e, Highlight h) {
-        Log.i("Entry selected", e.toString());
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu_with_scan, menu);
+        rescanButton = menu.findItem(R.id.rescanMenuButton);
+        rescanButton.setVisible(true);
+        return true;
     }
 
     @Override
-    public void onNothingSelected() {
-        Log.i("Nothing selected", "Nothing selected.");
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.settingsMenuButton:
+                //your action
+                break;
+            case R.id.rescanMenuButton:
+                alertBox("Choose wifi or Bluetooth", "PCstatus need a connection via WiFi or bluetooth\n" +
+                        "What you want to use?", REQUEST_WIFI_OR_BLUETOOTH);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 
-    private LineDataSet createSet() {
-
-        LineDataSet set = new LineDataSet(null, "Dynamic Data");
-        set.setAxisDependency(AxisDependency.LEFT);
-        set.setColor(ColorTemplate.getHoloBlue());
-        set.setCircleColor(Color.WHITE);
-        set.setLineWidth(2f);
-        set.setCircleRadius(4f);
-        set.setFillAlpha(65);
-        set.setFillColor(ColorTemplate.getHoloBlue());
-        set.setHighLightColor(Color.rgb(244, 117, 117));
-        set.setValueTextColor(Color.WHITE);
-        set.setValueTextSize(9f);
-        set.setDrawValues(false);
-        return set;
-    }
-
-    private void addEntry() {
-
-        LineData data = mChart.getData();
-
-        if (data != null) {
-
-            ILineDataSet set = data.getDataSetByIndex(0);
-            // set.addEntry(...); // can be called as well
-
-            if (set == null) {
-                set = createSet();
-                data.addDataSet(set);
-            }
-
-            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
-            data.notifyDataChanged();
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(15);
-            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
-
-            // move to the latest entry
-            mChart.moveViewToX(data.getEntryCount());
-
-            // this automatically refreshes the chart (calls invalidate())
-            // mChart.moveViewTo(data.getXValCount()-7, 55f,
-            // AxisDependency.LEFT);
+    public void setConnectionFlag(boolean b) {
+        connectionFlag = b;
+        if (connectionFlag) {
+            rescanButton.setVisible(false);
+        } else {
+            rescanButton.setVisible(true);
         }
     }
 }
